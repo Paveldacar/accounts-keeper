591caa22a4225394e3c46f6343db627845307aa3
Paveldacar <paveldacar@gmail.com>
2014-09-28 23:44:22 +0200

    Add routes to handle expenses, and manage date to use current month and year by default for the main route

--- src/Paveldacar/AccountsKeeperBundle/Controller/ExpenseController.php
+++ src/Paveldacar/AccountsKeeperBundle/Controller/ExpenseController.php
@@ -7,12 +7,58 @@ use Symfony\Component\HttpFoundation\Response;
 
 class ExpenseController extends Controller
 {
-    public function seeAllAction()
+    public function seeAllAction($month, $year)
     {
+        if ($month == null) {
+            $month = date('m');
+        }
+
+        $month = $month - 1;
+
+        $months = [
+            'janvier',
+            'février',
+            'mars',
+            'avril',
+            'mai',
+            'juin',
+            'juillet',
+            'août',
+            'septembre',
+            'octobre',
+            'novembre',
+            'décembre'
+        ];
+
+        if ($year == null) {
+            $year = date('Y');
+        }
+
         $content = $this->get('templating')->render('PaveldacarAccountsKeeperBundle:Expense:seeAll.html.twig', [
-            'month' => 'septembre',
-            'user'  => 'Paul'
+            'month' => $months[$month],
+            'year'  => $year
         ]);
+
         return new Response($content);
+    }
+
+    public function seeExpenseAction($id)
+    {
+        return new Response("Affichage de la dépense d'id : " . $id);
+    }
+
+    public function addAction()
+    {
+        return new Response("Cette page permettra l'ajout d'une nouvelle dépense à l'aide d'un formulaire.");
+    }
+
+    public function editAction($id)
+    {
+        return new Response("Cette page permettra de modifier la dépense d'id " . $id . " à l'aide d'un formulaire.");
+    }
+
+    public function deleteAction($id)
+    {
+        return new Response("Cette page permettra de supprimer la dépense d'id " . $id . ".");
     }
 }
